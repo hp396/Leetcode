273167dcf3585e3a59614d668503194baced3939
hp396 <48489886+hp396@users.noreply.github.com>
2020-01-20 00:56:01 -0500

    441. Arranging Coins
    Runtime: 8 ms, faster than 42.17% of Java online submissions for Arranging Coins.
    Memory Usage: 39.4 MB, less than 5.26% of Java online submissions for Arranging Coins.

---
+++ 441 Arranging Coins.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int arrangeCoins(int n) {
+        int counter = 0;
+        for(int i = 1; n>=0;i++){
+            n=n-i;
+            if(n>=0){
+                counter++;
+            }            
+        }
+        return counter;
+    }
+}
