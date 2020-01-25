d7ce5ed99798c2f49b188ce72fe27cb33f6637d7
hp396 <48489886+hp396@users.noreply.github.com>
2020-01-25 18:06:18 -0500

    Delete Solution.java

--- 41. First Missing Positive/Solution.java
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def firstMissingPositive(self, nums: List[int]) -> int:
-        x = int(len(nums))+2
-        nums = set(nums)
-        for i in range(1,x):
-            if i not in nums:
-                return i
